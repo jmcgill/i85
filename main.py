366c6a12e186e3dce25f976d948ca989ca7c0532
James McGill <plexer@gmail.com>
2012-12-01 12:37:31 +1100

    Initial import

---
+++ main.py
@@ -0,0 +1,192 @@
+# The main entry point for the server which drives i-85.
+
+###############################################################################
+## System Libraries
+###############################################################################
+
+import os
+import re
+import time
+import json
+import random
+import jinja2
+import hashlib
+
+from datetime import datetime
+
+###############################################################################
+## GAE Libraries
+###############################################################################
+
+import webapp2
+from google.appengine.ext import db
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+###############################################################################
+## User Libraries
+###############################################################################
+
+import models
+
+###############################################################################
+# Setup
+###############################################################################
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+###############################################################################
+# Views
+###############################################################################
+
+def ParseDate(date_string):
+  if date_string:
+    date = datetime.strptime(
+        date_string,
+        "%d-%m-%Y")
+    epoch = datetime(1970, 1, 1)
+    return (date - epoch).days
+  else:
+    return 0
+
+class Index(webapp2.RequestHandler):
+  def get(self):
+    q = db.GqlQuery("SELECT * FROM Task")
+    tasks = []
+    for task in q:
+      task.id = task.key().id()
+      if task.start_day is None:
+        task.start_day = 0
+      tasks.append(task)
+
+    q = db.GqlQuery("SELECT * FROM Vacation")
+    vacations = []
+    for vacation in q:
+      vacation.id = vacation.key().id()
+      if vacation.start_day is None:
+        vacation.start_day = 0
+      if vacation.end_day is None:
+        vacation.end_day = 0
+      vacations.append(vacation)
+
+    editable = 'false'
+    if self.request.get('edit'):
+      editable = 'true'
+
+    data = {
+      "tasks": tasks,
+      "vacations": vacations,
+      "editable": editable
+    }
+
+    template = jinja_environment.get_template('main.html')
+    self.response.out.write(template.render(data))
+
+
+class UpdateTask(webapp2.RequestHandler):
+  def post(self):
+    if (self.request.get('id')):
+      task = models.Task.get_by_id(int(self.request.get('id')))
+    else:
+      task = models.Task()
+
+    task.start_day = ParseDate(self.request.get('start_day'))
+    task.owner = self.request.get('owner')
+    task.description = self.request.get('description')
+    task.length = self.request.get('length')
+    task.dependent_tasks = self.request.get('dependent_tasks')
+
+    # Keep a history of 'days remaining' estimates.
+    now = str(int(time.time()))
+    if task.remaining:
+      remaining = task.remaining.split(',')
+      if remaining[-1] != self.request.get('remaining'):
+        task.remaining += "," + self.request.get('remaining')
+        task.remaining_updates += "," + now
+    else:
+      task.remaining = self.request.get('remaining')
+      task.remaining_updates = now
+
+    task.put()
+
+    output = {}
+    output["id"] = str(task.key().id())
+    output["owner"] = task.owner
+    output["description"] = task.description
+    output["start_day"] = task.start_day
+    output["remaining"] = task.remaining.split(",")
+    output["remaining_updates"] = task.remaining_updates.split(",")
+    output["length"] = task.length
+    output["dependent_tasks"] = task.dependent_tasks.split(",")
+
+    self.response.out.write(json.dumps(output))
+
+class DeleteTask(webapp2.RequestHandler):
+  def post(self):
+    if not (self.request.get('id')):
+      return
+
+    task = models.Task.get_by_id(int(self.request.get('id')))
+    db.delete(task)
+
+class UpdateVacation(webapp2.RequestHandler):
+  def post(self):
+    if (self.request.get('id')):
+      vacation = models.Vacation.get_by_id(int(self.request.get('id')))
+    else:
+      vacation = models.Vacation()
+
+    vacation.start_day = ParseDate(self.request.get('start_day'))
+    vacation.end_day = ParseDate(self.request.get('end_day'))
+    vacation.owner = self.request.get('owner')
+    vacation.put()
+
+    output = {}
+    output["id"] = str(vacation.key().id())
+    output["owner"] = vacation.owner
+    output["start_day"] = vacation.start_day
+    output["end_day"] = vacation.end_day
+
+    self.response.out.write(json.dumps(output))
+
+class DeleteVacation(webapp2.RequestHandler):
+  def post(self):
+    if not (self.request.get('id')):
+      return
+
+    vacation = models.Vacation.get_by_id(int(self.request.get('id')))
+    db.delete(vacation)
+
+class ViewBurndown(webapp2.RequestHandler):
+  def get(self, id):
+    task = models.Task.get_by_id(int(id))
+    remaining = task.remaining.split(",")
+    timestamps = task.remaining_updates.split(",")
+
+    burndown = []
+    for i in range(0, len(remaining)):
+      entry = {
+        "timestamp": timestamps[i],
+        "days_remaining": remaining[i]
+      }
+      burndown.append(entry)
+    
+    data = {
+      "task": task,
+      "burndown": burndown
+    }
+    template = jinja_environment.get_template('burndown.html')
+    self.response.out.write(template.render(data))
+
+app = webapp2.WSGIApplication([
+    ('/', Index),
+    ('/burndown/(\d+)', ViewBurndown),
+    ('/tasks/update', UpdateTask),
+    ('/tasks/delete', DeleteTask),
+    ('/vacations/update', UpdateVacation),
+    ('/vacations/delete', DeleteVacation),
+  ],
+  debug = True)
---
+++ models.py
@@ -0,0 +1,46 @@
+# Database models for i-85.
+
+from google.appengine.ext import db
+
+###############################################################################
+## MODELS
+###############################################################################
+
+# The state for a task.
+class Task(db.Model):
+  # The owner of this task.
+  owner = db.StringProperty()
+  
+  # A detailed description of the task.
+  description = db.StringProperty()
+  
+  # The day on which the task is being started, if known.
+  # Stored as an integer relative to Jan 1, 1970.
+  start_day = db.IntegerProperty()
+ 
+  # The number of days estimated for the task.
+  # TODO(jmcgill): Change into a number.
+  length = db.StringProperty()
+
+  # Comma separated history of days remaining.
+  remaining = db.StringProperty()
+
+  # Comma separated history of times at which days remaining was updated,
+  # in seconds since epoch.
+  remaining_updates = db.StringProperty()
+
+  # A list of the IDs of dependent tasks.
+  dependent_tasks = db.StringProperty()
+
+# The state for a vacation.
+class Vacation(db.Model):
+  # The owner or entity affected.
+  owner = db.StringProperty()
+  
+  # The day on which the vacation is being started, inclusive.
+  # Stored as an integer relative to Jan 1, 1970.
+  start_day = db.IntegerProperty()
+
+  # The day on which the vacation is being ended, inclusive.
+  # Stored as an integer relative to Jan 1, 1970.
+  end_day = db.IntegerProperty()
